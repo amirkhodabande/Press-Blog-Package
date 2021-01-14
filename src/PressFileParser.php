906774ecf65250a9253d82bd5dcb9a92b0293444
Amir Khodabande <amirdp8102@gmail.com>
2021-01-14 15:03:52 +0330

    Add rawData property to save the user content in it as array and all processed data will save in data property

--- src/PressFileParser.php
+++ src/PressFileParser.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\File;
 class PressFileParser
 {
     protected $filename;
+    protected $rawData;
     protected $data;
 
     public function __construct($filename)
@@ -28,22 +29,27 @@ class PressFileParser
         return $this->data;
     }
 
+    public function getRawData()
+    {
+        return $this->rawData;
+    }
+
     protected function splitFile()
     {
         preg_match('/^\-{3}(.*?)\-{3}(.*)/s',
             File::exists($this->filename) ? File::get($this->filename) : $this->filename,
-            $this->data
+            $this->rawData
         );
     }
 
     protected function explodeData()
     {
-        foreach (explode("\r", trim($this->data[1])) as $fieldString) {
+        foreach (explode("\r", trim($this->rawData[1])) as $fieldString) {
             preg_match('/(.*):\s?(.*)/', $fieldString, $fieldArray);
             $this->data[$fieldArray[1]] = $fieldArray[2];
         }
 
-        $this->data['body'] = trim($this->data[2]);
+        $this->data['body'] = trim($this->rawData[2]);
     }
 
     protected function processFields()
