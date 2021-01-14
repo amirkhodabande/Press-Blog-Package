41c815cac08a8dcf5c5ad046867369d1395fa5d3
Amir Khodabande <amirdp8102@gmail.com>
2021-01-14 13:13:10 +0330

    Add a simple test called MarkdownTest.php

---
+++ tests/Feature/MarkdownTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace amirgonvt\Press\Tests\Feature;
+
+
+use amirgonvt\Press\MarkdownParser;
+use Orchestra\Testbench\TestCase;
+
+class MarkdownTest extends TestCase
+{
+    /** @test **/
+    public function simple_markdown_is_parsed()
+    {
+        $this->assertEquals('<h1>Heading</h1>', MarkdownParser::parse('# Heading'));
+    }
+}
