931c716f1e8f0ddd298e6900586e9dd5577430e1
Amir Khodabande <amirdp8102@gmail.com>
2021-01-14 13:11:39 +0330

    Add PressFileParser.php

---
+++ src/PressFileParser.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace amirgonvt\Press;
+
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\File;
+
+class PressFileParser
+{
+    protected $filename;
+    protected $data;
+
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+
+        $this->splitFile();
+
+        $this->explodeData();
+
+        $this->processFields();
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    protected function splitFile()
+    {
+        preg_match('/^\-{3}(.*?)\-{3}(.*)/s',
+            File::exists($this->filename) ? File::get($this->filename) : $this->filename,
+            $this->data
+        );
+    }
+
+    protected function explodeData()
+    {
+        foreach (explode("\r", trim($this->data[1])) as $fieldString) {
+            preg_match('/(.*):\s?(.*)/', $fieldString, $fieldArray);
+            $this->data[$fieldArray[1]] = $fieldArray[2];
+        }
+
+        $this->data['body'] = trim($this->data[2]);
+    }
+
+    protected function processFields()
+    {
+        foreach ($this->data as $field => $value) {
+            if($field === 'date') {
+                $this->data[$field] = Carbon::parse($value);
+            } elseif ($field === 'body') {
+                $this->data[$field] = MarkdownParser::parse($value);
+            }
+        }
+    }
+
+}
