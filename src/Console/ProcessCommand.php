4ec2ba50fa61853702ca0786bca3e6cfa0eca6ad
Amir Khodabande <amirdp8102@gmail.com>
2021-01-16 16:38:32 +0330

    Use config file instead of hard coding data

--- src/Console/ProcessCommand.php
+++ src/Console/ProcessCommand.php
@@ -5,11 +5,11 @@ namespace amirgonvt\Press\Console;
 
 
 use amirgonvt\Press\Post;
+use amirgonvt\Press\Press;
 use amirgonvt\Press\PressFileParser;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 use \Illuminate\Support\Str;
-use function PHPUnit\Framework\isEmpty;
 
 class ProcessCommand extends Command
 {
@@ -19,7 +19,12 @@ class ProcessCommand extends Command
 
     public function handle()
     {
-        $files = File::files('blogs');
+        if (Press::configNotPublished()) {
+            return $this->warn("Please publish the config file by running" .
+                "\'php artisan vendor:publish --tag=press-config\'");
+        }
+
+        $files = File::files(config('press.path'));
 
         foreach ($files as $file) {
             $post = (new PressFileParser($file->getPathname()))->getData();
