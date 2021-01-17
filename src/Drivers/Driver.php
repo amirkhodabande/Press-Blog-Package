0a35396d18dffaa608d0297e51249af4a92bd6e6
Amir Khodabande <amirdp8102@gmail.com>
2021-01-17 14:06:41 +0330

    Add Driver.php

---
+++ src/Drivers/Driver.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace amirgonvt\Press\Drivers;
+
+
+use amirgonvt\Press\PressFileParser;
+use Illuminate\Support\Str;
+
+abstract class Driver
+{
+    protected $config;
+    protected $posts = [];
+
+    public function __construct()
+    {
+        $this->setConfig();
+
+        $this->validateSource();
+    }
+
+    public abstract function fetchPosts();
+
+    protected function setConfig()
+    {
+        $this->config = config('press.' . config('press.driver'));
+    }
+
+    protected function validateSource()
+    {
+        return true;
+    }
+
+    protected function parse($content, $identifier)
+    {
+        $this->posts[] = array_merge(
+            (new PressFileParser($content))->getData(),
+            ['identifier' => Str::slug($identifier)]
+        );
+    }
+}
