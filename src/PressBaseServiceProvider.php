caffd46a4baf27b4f2bf971cec91f02c64bbe152
Amir Khodabande <amirdp8102@gmail.com>
2021-01-17 16:46:40 +0330

    Add routes to our ServiceProvider

--- src/PressBaseServiceProvider.php
+++ src/PressBaseServiceProvider.php
@@ -3,16 +3,53 @@
 
 namespace amirgonvt\Press;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class PressBaseServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        if($this->app->runningInConsole()) {
+        if ($this->app->runningInConsole()) {
             $this->registerPublishing();
         }
         $this->registerResources();
+    }
+
+    protected function registerPublishing()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/press.php' => config_path('press.php'),
+        ], 'press-config');
+    }
+
+    private function registerResources()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'press');
+
+        $this->registerRoutes();
+    }
+
+    protected function registerRoutes()
+    {
+        Route::group($this->routeConfiguration(), function () {
+            $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        });
+    }
+
+    /**
+     * Routes configuration will set from here.
+     *
+     * @return array
+     */
+    private function routeConfiguration():array
+    {
+        return [
+            'prefix' => Press::path(),
+            'namespace' => 'amirgonvt\Press\Http\Controllers',
+        ];
     }
 
     public function register()
@@ -20,17 +57,5 @@ class PressBaseServiceProvider extends ServiceProvider
         $this->commands([
             Console\ProcessCommand::class,
         ]);
-    }
-
-    private function registerResources()
-    {
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-    }
-
-    protected function registerPublishing()
-    {
-        $this->publishes([
-            __DIR__.'/../config/press.php' => config_path('press.php'),
-        ], 'press-config');
     }
 }
