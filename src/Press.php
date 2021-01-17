428bcf2f27fb2837a01f830c25ecd0f0ad43f9f6
Amir Khodabande <amirdp8102@gmail.com>
2021-01-17 11:40:06 +0330

    Add driver function in Press.php

--- src/Press.php
+++ src/Press.php
@@ -6,8 +6,16 @@ namespace amirgonvt\Press;
 
 class Press
 {
-    public static function configNotPublished()
+    public static function configNotPublished(): bool
     {
         return is_null(config('press'));
+    }
+
+    public static function driver()
+    {
+        $driver = ucfirst(config('press.driver'));
+        $class = "amirgonvt\\Press\\Drivers\\" . $driver . "Driver";
+
+        return new $class;
     }
 }
