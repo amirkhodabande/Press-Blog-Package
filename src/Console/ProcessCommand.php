dbbd227a3bc80891a0812146cc38ce803570e061
Amir Khodabande <amirdp8102@gmail.com>
2021-01-17 11:40:47 +0330

    Refactor, add FileDriver

--- src/Console/ProcessCommand.php
+++ src/Console/ProcessCommand.php
@@ -24,32 +24,16 @@ class ProcessCommand extends Command
                 "\'php artisan vendor:publish --tag=press-config\'");
         }
 
-        $files = File::files(config('press.path'));
+       $posts = Press::driver()->fetchPosts();
 
-        foreach ($files as $file) {
-            $post = (new PressFileParser($file->getPathname()))->getData();
-            /*
-             * I dont know why : the post contain extra field with duplicate values from title and descriotion.
-             *
-             * so I decide to remove theme manually from here, if you know a better way please tell me :) */
-            $extra = json_decode($post['extra']);
-            unset($extra->title);
-            unset($extra->description);
-
-            $extra = json_encode($extra);
-            if ($extra !== "{}")
-                $post['extra'] = $extra;
-            else unset($post['extra']);
-
-        }
-        isset($post['extra']) ? $extra = $post['extra'] : [];
-
-        Post::create([
-            'identifier' => Str::random(),
-            'slug' => Str::slug($post['title']),
-            'title' => $post['title'],
-            'body' => $post['body'],
-            'extra' => $extra,
-        ]);
+       foreach ($posts as $post) {
+           Post::create([
+               'identifier' => Str::random(),
+               'slug' => Str::slug($post['title']),
+               'title' => $post['title'],
+               'body' => $post['body'],
+               'extra' => $post['extra'],
+           ]);
+       }
     }
 }
