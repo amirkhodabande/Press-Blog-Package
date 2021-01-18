77a6fc274aacfbe6fb8aa50d75e53780cbf0d37e
Amir Khodabande <amirdp8102@gmail.com>
2021-01-18 16:36:43 +0330

    Refactor, Add extra function to get any extra field

--- src/Repositories/PostRepository.php
+++ src/Repositories/PostRepository.php
@@ -11,13 +11,33 @@ class PostRepository
 {
     public function save($post)
     {
+        $post['extra'] = $this->unsetTitleFromExtra($post);
+
         Post::updateOrCreate([
             'identifier' => $post['identifier'],
         ], [
             'slug' => Str::slug($post['title']),
             'title' => $post['title'],
             'body' => $post['body'],
-            'extra' => $post['extra'] ?? json_encode([]),
+            'extra' => $this->extra($post),
         ]);
+    }
+
+    private function unsetTitleFromExtra($post)
+    {
+        $post['extra'] = json_decode($post['extra']);
+
+        unset($post['extra']->title);
+
+        return json_encode($post['extra']);
+    }
+
+    private function extra($post)
+    {
+        $extra = (array)json_decode($post['extra'] ?? '[]');
+
+        $attributes = array_except($post, ['title', 'body', 'identifier', 'extra']);
+
+        return json_encode(array_merge($extra, $attributes));
     }
 }
